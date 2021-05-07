1a8d5a04b7b2279670d4268961afecb9d6cb0039
S.Amelin <1.slava.1.amelin.1@gmail.com>
2021-05-08 01:14:11 +0300

    V 1.4

--- js/script.js
+++ js/script.js
@@ -1,3 +1,64 @@
-'Use strict';
+'use strict';
 
-//fwfewfe
+const switcher = document.querySelector('.btnTheme');
+
+switcher.addEventListener('click', function() {
+    document.body.classList.toggle('dark-theme');
+
+    let className = document.body.className;
+    if (className == "light-theme") {
+        this.textContent = "Dark";
+    } else {
+        this.textContent = "Light";
+    }
+    //console.log('current class name: ' + className);
+});
+
+//Modal
+
+const modalTrigger = document.querySelectorAll('[data-modal]'),
+    modal = document.querySelector('.modal'),
+    modalCloseBtn = document.querySelector('[data-close]');
+
+modalTrigger.forEach(btn => {
+    btn.addEventListener('click', function() {
+        modal.classList.add('show');
+        modal.classList.remove('hide');
+        // Либо вариант с toggle - но тогда назначить класс в верстке
+        document.body.style.overflow = 'hidden';
+    });
+});
+
+function closeModal() {
+    modal.classList.add('hide');
+    modal.classList.remove('show');
+    // Либо вариант с toggle - но тогда назначить класс в верстке
+    document.body.style.overflow = '';
+}
+
+modalCloseBtn.addEventListener('click', closeModal);
+
+modal.addEventListener('click', (e) => {
+    if (e.target === modal) {
+        closeModal();
+    }
+});
+
+document.addEventListener('keydown', (e) => {
+    if (e.code === "Escape" && modal.classList.contains('show')) {
+        closeModal();
+    }
+});
+// const modalTrigger = document.querySelectorAll('[data-modal]'),
+//     modal = document.querySelector('.modal'),
+//     modalCloseBtn = document.querySelector('[dara-close]');
+
+// modalTrigger.addEventListener('click', () => {
+//     modal.classList.add('show');
+//     modal.classList.remove('hide');
+// });
+
+// modalCloseBtn.addEventListener('click', () => {
+//     modal.classList.add('hide');
+//     modal.classList.remove('show');
+// });
