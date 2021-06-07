23b6e6e20327299d765a1668001862562372c2de
Artem Krutikov <artempozitiv56@gmail.com>
2021-06-07 17:26:10 +0300

    Some changes

--- js/advertisement.js
+++ js/advertisement.js
@@ -1,40 +1,40 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', () => {
-    const posts = JSON.parse(localStorage.getItem('posts'));
-    const container = document.querySelector('.catalog_example_ad2');
-    const href = document.location.href;
-    for (const post of posts) {
-        if (post.postId == href.split('?id=')[1]) {
-            const photos = addNameClass('photo_advertisment');
-            photos.innerHTML = `${post.photoPost[1]}`;
+  const posts = JSON.parse(localStorage.getItem('posts'));
+  const container = document.querySelector('.catalog_example_ad2');
+  const href = document.location.href;
+  for (const post of posts) {
+    if (post.postId == href.split('?id=')[1]) {
+      const photos = addNameClass('photo_advertisment');
+      photos.innerHTML = `${post.photoPost[1]}`;
 
-            const nameProduct = addNameClass('text_h3_post_add content_block');
-            nameProduct.innerText = `${post.productName}`;
+      const nameProduct = addNameClass('text_h3_post_add content_block');
+      nameProduct.innerText = `${post.productName}`;
 
-            const productPrice = addNameClass('text_h3_post_add');
-            productPrice.innerText = `Цена: ${post.productPrice}`;
+      const productPrice = addNameClass('text_h3_post_add');
+      productPrice.innerText = `Цена: ${post.productPrice}`;
 
-            const productDescription = addNameClass('text_h3_post_add content_block');
-            productDescription.innerText = `Описание \n ${post.productDescription}`;
+      const productDescription = addNameClass('text_h3_post_add content_block');
+      productDescription.innerText = `Описание \n ${post.productDescription}`;
 
-            const productContInfo = addNameClass('text_h3_post_add content_block');
-            productContInfo.innerText = `Контактная информация
+      const productContInfo = addNameClass('text_h3_post_add content_block');
+      productContInfo.innerText = `Контактная информация
             Город: ${post.nameCity}.
             Номер телефона: ${post.namePhone}
             Почта: ${post.nameEmail}`;
 
-            container.append(photos);
-            container.append(nameProduct);
-            container.append(productDescription);
-            container.append(productContInfo);
-            nameProduct.append(productPrice);
-        }
+      container.append(photos);
+      container.append(nameProduct);
+      container.append(productDescription);
+      container.append(productContInfo);
+      nameProduct.append(productPrice);
     }
+  }
 
-    function addNameClass(nameClass) {
-        const res = document.createElement('div');
-        res.className = `${nameClass}`;
-        return res;
-    }
+  function addNameClass(nameClass) {
+    const res = document.createElement('div');
+    res.className = `${nameClass}`;
+    return res;
+  }
 });
--- js/likeBtn.js
+++ js/likeBtn.js
@@ -1,49 +1,49 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', () => {
-    // .........Кнопка Лайка............
+  // .........Кнопка Лайка............
 
-    const btnLike = document.querySelectorAll('.work_btn_like');
-    const user = document.cookie.split('[')[1].split(']')[0];
-    let like = [];
-    if (user != '') {
-        like = user.split(',');
-    }
-    for (const btn of btnLike) {
-        btn.addEventListener('click', () => {
-            btn.classList.toggle('work_btn_like2');
-            const counBtnLike = btn.querySelector('.like_btn_counter');
-            const className = btn.className;
-            for (let i = 0; i < btnLike.length; i++) {
-                if (btnLike[i] === btn) {
-                    if (className === 'work_btn_like') {
-                        like = spliceArr(counBtnLike, like, i);
-                    } else {
-                        like = sliceArr(counBtnLike, like, i);
-                    }
-                    document.cookie =
+  const btnLike = document.querySelectorAll('.work_btn_like');
+  const user = document.cookie.split('[')[1].split(']')[0];
+  let like = [];
+  if (user != '') {
+    like = user.split(',');
+  }
+  for (const btn of btnLike) {
+    btn.addEventListener('click', () => {
+      btn.classList.toggle('work_btn_like2');
+      const counBtnLike = btn.querySelector('.like_btn_counter');
+      const className = btn.className;
+      for (let i = 0; i < btnLike.length; i++) {
+        if (btnLike[i] === btn) {
+          if (className === 'work_btn_like') {
+            like = spliceArr(counBtnLike, like, i);
+          } else {
+            like = sliceArr(counBtnLike, like, i);
+          }
+          document.cookie =
                         `${document.cookie.split('[')[0]}[${like}]${document.cookie.split(']')[1]}`;
-                }
-            }
-        });
-    }
-
-    function spliceArr(counBtnLike, like, i) {
-        counBtnLike.textContent--;
-        for (let j = 0; j < like.length; j++) {
-            if (i === like[j]) {
-                return like.splice(j, 1);
-            }
         }
-    }
+      }
+    });
+  }
 
-    function sliceArr(counBtnLike, like, i) {
-        counBtnLike.textContent++;
-        const insert = (arr, index, newItem) => [
-            ...arr.slice(0, index),
-            newItem,
-            ...arr.slice(index),
-        ];
-        return insert(like, i, i);
+  function spliceArr(counBtnLike, like, i) {
+    counBtnLike.textContent--;
+    for (let j = 0; j < like.length; j++) {
+      if (i === like[j]) {
+        return like.splice(j, 1);
+      }
     }
+  }
+
+  function sliceArr(counBtnLike, like, i) {
+    counBtnLike.textContent++;
+    const insert = (arr, index, newItem) => [
+      ...arr.slice(0, index),
+      newItem,
+      ...arr.slice(index),
+    ];
+    return insert(like, i, i);
+  }
 });
