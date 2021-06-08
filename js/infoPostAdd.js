01159f8854d002b5f714941de49547c9cf26a233
S.Amelin <1.slava.1.amelin.1@gmail.com>
2021-06-08 23:11:53 +0300

    Some Changes

--- js/infoPostAdd.js
+++ js/infoPostAdd.js
@@ -1,84 +1,84 @@
 'use strict';
 
 document.addEventListener('DOMContentLoaded', () => {
-  /*Category window*/
+    /*Category window*/
 
-  const selected = document.querySelector('.selected');
-  const optionsContainer = document.querySelector('.options-container');
-  const searchBox = document.querySelector('.search-box input');
-  const optionsList = document.querySelectorAll('.option');
+    const selected = document.querySelector('.selected');
+    const optionsContainer = document.querySelector('.options-container');
+    const searchBox = document.querySelector('.search-box input');
+    const optionsList = document.querySelectorAll('.option');
 
-  const filterList = searchTerm => {
-    searchTerm = searchTerm.toLowerCase();
-    for (const option of optionsList) {
-      const label =
+    const filterList = searchTerm => {
+        searchTerm = searchTerm.toLowerCase();
+        for (const option of optionsList) {
+            const label =
                 option.firstElementChild.nextElementSibling.innerText.toLowerCase();
-      label.indexOf(searchTerm) !== -1 ?
-        (option.style.display = 'block') :
-        (option.style.display = 'none');
-    }
-  };
+            label.indexOf(searchTerm) !== -1 ?
+                (option.style.display = 'block') :
+                (option.style.display = 'none');
+        }
+    };
 
-  selected.addEventListener('click', () => {
-    optionsContainer.classList.toggle('active');
+    selected.addEventListener('click', () => {
+        optionsContainer.classList.toggle('active');
 
-    searchBox.value = '';
-    filterList('');
+        searchBox.value = '';
+        filterList('');
 
-    if (optionsContainer.classList.contains('active')) {
-      searchBox.focus();
-    }
-  });
-
-  for (const oplis of optionsList) {
-    oplis.addEventListener('click', () => {
-      selected.innerHTML = oplis.querySelector('label').innerHTML;
-      optionsContainer.classList.remove('active');
+        if (optionsContainer.classList.contains('active')) {
+            searchBox.focus();
+        }
     });
-  }
 
-  searchBox.addEventListener('keyup', e => {
-    filterList(e.target.value);
-  });
+    for (const oplis of optionsList) {
+        oplis.addEventListener('click', () => {
+            selected.innerHTML = oplis.querySelector('label').innerHTML;
+            optionsContainer.classList.remove('active');
+        });
+    }
 
-  /*...........Counter of letters...........*/
+    searchBox.addEventListener('keyup', e => {
+        filterList(e.target.value);
+    });
 
-  const myInput = document.getElementById('my-input');
-  const myTextArea = document.getElementById('my-textarea');
-  const remainingCharsInput = document.getElementById(
-    'my-input-remaining-chars'
-  );
-  const remainingCharsTextArea = document.getElementById(
-    'my-textarea-remaining-chars'
-  );
-  const MAX_CHARS = 500;
-  const MAX_CHARS_INPUT = 70;
+    /*...........Counter of letters...........*/
 
-  myTextArea.addEventListener('input', () => {
-    const remaining = myTextArea.value.length;
-    remainingCharsTextArea.textContent = `${remaining}/${MAX_CHARS}`;
-  });
+    const myInput = document.getElementById('my-input');
+    const myTextArea = document.getElementById('my-textarea');
+    const remainingCharsInput = document.getElementById(
+        'my-input-remaining-chars'
+    );
+    const remainingCharsTextArea = document.getElementById(
+        'my-textarea-remaining-chars'
+    );
+    const MAX_CHARS = 500;
+    const MAX_CHARS_INPUT = 70;
 
-  myInput.addEventListener('input', () => {
-    const remaining = myInput.value.length;
-    remainingCharsInput.textContent = `${remaining}/ ${MAX_CHARS_INPUT}`;
-  });
+    myTextArea.addEventListener('input', () => {
+        const remaining = myTextArea.value.length;
+        remainingCharsTextArea.textContent = `${remaining}/${MAX_CHARS}`;
+    });
 
-  myTextArea.setAttribute('maxlength', MAX_CHARS);
-  myInput.setAttribute('maxlength', MAX_CHARS_INPUT);
+    myInput.addEventListener('input', () => {
+        const remaining = myInput.value.length;
+        remainingCharsInput.textContent = `${remaining}/ ${MAX_CHARS_INPUT}`;
+    });
 
-  //.........................Добавление фото..............
+    myTextArea.setAttribute('maxlength', MAX_CHARS);
+    myInput.setAttribute('maxlength', MAX_CHARS_INPUT);
 
-  const popa = document.querySelector('.contant_photo_add');
-  let filePhotos;
-  let countClick = 0;
-  const maxAddPhoto = 5;
-  const array = [];
+    //.........................Добавление фото..............
 
-  function printBlockPhoto() {
-    const nemPhoto = document.createElement('div');
-    nemPhoto.className = 'add_photos';
-    nemPhoto.innerHTML = `
+    const popa = document.querySelector('.contant_photo_add');
+    let filePhotos;
+    let countClick = 0;
+    const maxAddPhoto = 5;
+    const array = [];
+
+    function printBlockPhoto() {
+        const nemPhoto = document.createElement('div');
+        nemPhoto.className = 'add_photos';
+        nemPhoto.innerHTML = `
 			<button class='btn_add_photos'>
 				<svg id="upload-photo-icon" width="45" height="45" viewBox="0 0 35 35" fill="none" xmlns="http://www.w3.org/2000/svg">
 					<path fill-rule="evenodd" clip-rule="evenodd" d="M10.4913 7.04599C11.0775 5.98064 11.7152 5.26846 12.6614 4.89184C14.3954 4.20237 20.6055 4.20237 22.3381 4.89198C23.2839 5.2684 23.9218 5.98045 24.5084 7.04558L25.1679 8.31018C25.183 8.33523 25.1969 8.35727 25.2093 8.37623L25.2653 8.4528C25.5658 8.7802 26.0686 8.97799 26.4005 8.97799C29.5389 8.97799 32.0837 11.5222 32.0837 14.6597V23.1107C32.0837 27.1127 28.8377 30.3586 24.8357 30.3586H10.1649C6.16194 30.3586 2.91699 27.1131 2.91699 23.1107V14.6597C2.91699 11.5222 5.46175 8.97799 8.60012 8.97799C8.93083 8.97799 9.4344 8.77991 9.73534 8.4528C9.74135 8.44625 9.7492 8.43649 9.75869 8.42358L9.83368 8.30854L10.4913 7.04599ZM22.5923 8.10092C22.2278 7.43907 21.8984 7.07135 21.5292 6.92441L21.3552 6.86657C19.8543 6.44236 14.6338 6.46164 13.47 6.92441C13.101 7.07128 12.7719 7.4388 12.4079 8.10052L11.8466 9.18566L11.7085 9.43566C11.5931 9.62747 11.4778 9.78939 11.3461 9.9329C10.6075 10.7357 9.51486 11.1655 8.60012 11.1655L8.36081 11.1736C6.54205 11.2965 5.10449 12.8108 5.10449 14.6597V23.1107C5.10449 25.9051 7.37014 28.1711 10.1649 28.1711H24.8357C27.6296 28.1711 29.8962 25.9046 29.8962 23.1107V14.6597C29.8962 12.7304 28.3309 11.1655 26.4005 11.1655L26.1466 11.1549C25.2883 11.0843 24.3243 10.6626 23.6537 9.93193C23.5237 9.79032 23.409 9.62956 23.2944 9.43936C23.2779 9.41208 23.2575 9.37616 23.234 9.33366L23.0647 9.01611L22.5923 8.10092Z"/>
@@ -88,109 +88,111 @@ document.addEventListener('DOMContentLoaded', () => {
 			</button>
 			<input class="file_input_photos" type="file" hidden>
 		`;
-    popa.append(nemPhoto);
-    return nemPhoto;
-  }
-  printBlockPhoto();
+        popa.append(nemPhoto);
+        return nemPhoto;
+    }
+    printBlockPhoto();
 
-  let dropPhotoAdd = document.querySelectorAll('.add_photos');
-  const btnAddPhotos =
+    let dropPhotoAdd = document.querySelectorAll('.add_photos');
+    const btnAddPhotos =
         dropPhotoAdd[countClick].querySelectorAll('.btn_add_photos');
-  const fileInput =
+    const fileInput =
         dropPhotoAdd[countClick].querySelector('.file_input_photos');
 
 
-  for (const btn of btnAddPhotos) {
+    for (const btn of btnAddPhotos) {
 
-    btn.addEventListener('click', () => {
-      if (countClick < maxAddPhoto) {
-        fileInput.click(); //если пользователь нажимает кнопку, то ввод также нажимаеться
-        printBlockPhoto();
-        dropPhotoAdd = document.querySelectorAll('.add_photos');
-        countClick++;
-      }
+        btn.addEventListener('click', () => {
+            if (countClick < maxAddPhoto) {
+                fileInput.click(); //если пользователь нажимает кнопку, то ввод также нажимаеться
+                printBlockPhoto();
+                dropPhotoAdd = document.querySelectorAll('.add_photos');
+                countClick++;
+            }
+        });
+    }
+
+    fileInput.addEventListener('change', function() {
+        filePhotos = this.files[0]; //если выбрали несколько файлов, то берём первый
+        showFile();
     });
-  }
 
-  fileInput.addEventListener('change', function() {
-    filePhotos = this.files[0]; //если выбрали несколько файлов, то берём первый
-    showFile();
-  });
-
-  function showFile() {
-    const fileType = filePhotos.type; //получение выбранного типа файла
-    const validExtensions = ['image/jpeg', 'image/jpg', 'image/png']; //формат фото
-    if (validExtensions.includes(fileType)) { //Если файл изображение
-      const fileReader = new FileReader(); //созаём новый FileReader
-      fileReader.onload = () => {
-        const fileURL = fileReader.result; //передача источника файла пользователя в fileURL
-        const imgTag = `<img src="${fileURL}" alt="">`; //создание тега img и передача в атрибута src
-        dropPhotoAdd[countClick].innerHTML = imgTag; //добавление созданного тега img внутри контейнера dropPhotoAdd
-        array.push(imgTag);
-      };
-      fileReader.readAsDataURL(filePhotos);
-    } else {
-      alert('Это не изображение!');
+    function showFile() {
+        const fileType = filePhotos.type; //получение выбранного типа файла
+        const validExtensions = ['image/jpeg', 'image/jpg', 'image/png']; //формат фото
+        if (validExtensions.includes(fileType)) { //Если файл изображение
+            const fileReader = new FileReader(); //созаём новый FileReader
+            fileReader.onload = () => {
+                const fileURL = fileReader.result; //передача источника файла пользователя в fileURL
+                const imgTag = `<img src="${fileURL}" alt="">`; //создание тега img и передача в атрибута src
+                dropPhotoAdd[countClick].innerHTML = imgTag; //добавление созданного тега img внутри контейнера dropPhotoAdd
+                array.push(imgTag);
+            };
+            fileReader.readAsDataURL(filePhotos);
+        } else {
+            alert('Это не изображение!');
+        }
     }
-  }
 
-  //.................Добавление поста....................
+    //.................Добавление поста....................
 
-  const nameAccount = document.querySelector('.input_name_account').value;
-  const nameEmail = document.querySelector('.input_email').value;
-  const namePhone = document.querySelector('.input_phone').value;
+    const nameAccount = document.querySelector('.input_name_account').value;
+    const nameEmail = document.querySelector('.input_email').value;
+    const namePhone = document.querySelector('.input_phone').value;
 
-  const namePattern = /\w \w \w/;
-  const numberPattern = /[0-9]/;
-  const emailPattern = /[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{1,63}$/;
+    const namePattern = /\w \w \w/;
+    const numberPattern = /[0-9]/;
+    const emailPattern = /[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{1,63}$/;
 
-  function validForm(name, rx) {
-    return rx.test(name);
-  }
-
-  function inputText() {
-    let flag = true;
-    const field = document.querySelectorAll('.input_post_add');
-    for (const item of field) {
-      if (item.value && validForm(nameAccount, namePattern) && validForm(nameEmail, numberPattern) && validForm(namePhone, emailPattern)) {
-        document.getElementById('submit').href = '../index.html';
-        flag = true;
-        return gettingInfo(flag);
-      }
-      item.style.borderColor = 'red';
-      flag = false;
-      return gettingInfo(flag);
+    function validForm(name, rx) {
+        return rx.test(name);
     }
-  }
 
-  function gettingInfo(flag) {
-    const posts = localStorage.getItem('posts');
-    const postInfo = {
-      postId: 0,
-      productLike: 228,
-    };
-
-    if (flag) {
-      postInfo.productName = document.querySelector('.input_poduct_name').value;
-      postInfo.productPrice = document.querySelector('.input_poduct_price').value;
-      postInfo.productDescription = document.querySelector('.input_product_description').value;
-      postInfo.productCatalog = document.querySelector('.input_product_catalog').value;
-      postInfo.nameCity = document.querySelector('.input_name_city').value;
-      postInfo.nameAccount = nameAccount;
-      postInfo.nameEmail = nameEmail;
-      postInfo.namePhone = namePhone;
-      postInfo.photoPost = array;
-
-      if (posts) {
-        const parsed = JSON.parse(posts);
-        postInfo.postId = parsed.length;
-        parsed.push(postInfo);
-        localStorage.setItem('posts', JSON.stringify(parsed));
-      } else {
-        localStorage.setItem('posts', JSON.stringify([postInfo]));
-      }
+    function inputText() {
+        let flag = true;
+        const field = document.querySelectorAll('.input_post_add');
+        for (const item of field) {
+            if (item.value && validForm(nameAccount, namePattern) &&
+                validForm(nameEmail, numberPattern) &&
+                validForm(namePhone, emailPattern)) {
+                document.getElementById('submit').href = '../index.html';
+                flag = true;
+                return gettingInfo(flag);
+            }
+            item.style.borderColor = 'red';
+            flag = false;
+            return gettingInfo(flag);
+        }
     }
-  }
 
-  document.getElementById('publish_post').addEventListener('click', inputText);
+    function gettingInfo(flag) {
+        const posts = localStorage.getItem('posts');
+        const postInfo = {
+            postId: 0,
+            productLike: 228,
+        };
+
+        if (flag) {
+            postInfo.productName = document.querySelector('.input_poduct_name').value;
+            postInfo.productPrice = document.querySelector('.input_poduct_price').value;
+            postInfo.productDescription = document.querySelector('.input_product_description').value;
+            postInfo.productCatalog = document.querySelector('.input_product_catalog').value;
+            postInfo.nameCity = document.querySelector('.input_name_city').value;
+            postInfo.nameAccount = nameAccount;
+            postInfo.nameEmail = nameEmail;
+            postInfo.namePhone = namePhone;
+            postInfo.photoPost = array;
+
+            if (posts) {
+                const parsed = JSON.parse(posts);
+                postInfo.postId = parsed.length;
+                parsed.push(postInfo);
+                localStorage.setItem('posts', JSON.stringify(parsed));
+            } else {
+                localStorage.setItem('posts', JSON.stringify([postInfo]));
+            }
+        }
+    }
+
+    document.getElementById('publish_post').addEventListener('click', inputText);
 });
